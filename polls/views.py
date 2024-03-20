750478e3d6231812c1950a7aa959a8f98ec160c1
Leonardo Souza <leonardo.castrodesouza@gmail.com>
2024-03-20 16:19:04 -0300

    Commit da criação de Templates

--- polls/views.py
+++ polls/views.py
@@ -7,7 +7,8 @@ from django.http import HttpResponse
 # define um view baseado em função
 def index(request):
     # return HttpResponse("Django - Retorno na url -> views Index")
-    return render(request, 'index.html')
+    # return render(request, 'index.html')
+    return render(request, 'index.html', {'titulo': 'Últimas Enquetes'})
 
 # define uma view baseada em função
 def ola(request):
--- projeto/settings.py
+++ projeto/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -122,3 +123,9 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+# Cria os arquivos estaticos configurando a segurança
+
+STATIC_ROOT = os.path.join(BASE_DIR, "public")
+STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
